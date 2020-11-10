ce5f1edc6e4d842dba41e21ba4833d5b47d376e8
Donne <17685811513@163.com>
2020-11-10 17:36:55 +0800

    ADD:CommitTestMaster

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * 2 * @Author: donne
+ * 3 * @Date: 2020/11/10 17:36
+ * 4
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test Master Commit ！");
+    }
+}
